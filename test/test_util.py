f2195ec9c25c9eaae3e293981718eacda7349f34
Nikita Unkovsky <nii.unkovsky@gmail.com>
2021-10-13 19:47:17 +0300

    Remove unused test code

--- test/test_util.py
+++ test/test_util.py
@@ -2,11 +2,11 @@ from typing import List
 from kantele import Circuit, qubit
 
 
-def prepare_circuit(qubits: list) -> Circuit:
-    circuit = Circuit(len(qubits))
-    for position, qubit in enumerate(qubits):
-        circuit.set_qubit(position, qubit)
-    return circuit
+# def prepare_circuit(qubits: list) -> Circuit:
+#     circuit = Circuit(len(qubits))
+#     for position, qubit in enumerate(qubits):
+#         circuit.set_qubit(position, qubit)
+#     return circuit
 
 
 def prepare_circuit(qubits: List[int]) -> Circuit:
